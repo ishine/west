85c5555846658a4cde480a26c2a8db03925d8e62
Binbin Zhang <binbzha@qq.com>
2025-06-19 12:36:39 +0000

    [models] add missing model.py

---
+++ west/models/model.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2025 Binbin Zhang(binbzha@qq.com)
+
+from dataclasses import dataclass
+
+
+@dataclass
+class ModelArgs:
+    model_type: str = 'speech_llm'
+
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    @classmethod
+    def register(cls, data_class):
+        cls.__dataclass_fields__.update(data_class.__dataclass_fields__)
+        cls.__annotations__.update(data_class.__annotations__)
+        return data_class
+
+
+class Model:
+    model_type = 'model'
+    _registry = {}
+
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        cls._registry[cls.model_type.lower()] = cls
+
+    @classmethod
+    def get_class(cls, model_type):
+        return cls._registry[model_type]
